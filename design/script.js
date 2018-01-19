b2d67ed268269c22ff1ac54ac562cf9c16ea1de8
GSargsyan <g.sargsyan1995@gmail.com>
2018-01-19 10:39:52 +0400

    Hour hand moving logic

--- design/script.js
+++ design/script.js
@@ -4,18 +4,26 @@ var currentSecond = now.getSeconds();
 var currentHour = now.getHours();
 
 var minuteDegrees = 0.1 * now.getSeconds() + 6 * now.getMinutes();
-var hourDegrees = now.getHours() * 360 / 24;
+var hourDegrees = 30 * now.getHours() + now.getMinutes() * 0.5;
+
+var tickInterval = 100; // milliseconds
 
 function init_clock() {
-	setInterval("tickMinutes()", 100);
+	setInterval("tickClock()", tickInterval);
 }
 
 document.body.onload = init_clock();
 
-function tickMinutes() {
+function tickClock() {
 	var minuteHand = byId("clock-minute-hand");
+	var hourHand = byId("clock-hour-hand");
+
 	minuteHand.style.transform = "rotate(" + minuteDegrees + "deg)";
+	hourHand.style.transform = "rotate(" + hourDegrees + "deg)";
+
 	minuteDegrees += 0.01;
+	// TODO: bad solution, rething this part
+	hourDegrees += 0.0083333334;
 }
 
 function byId(id) {
